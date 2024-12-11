662287ff50c321f636a5ab96cdec91322d99cc65
[Noah <[noah.melaku2@student.tdsb.on.ca]>
2024-12-11 15:17:03 -0500

    push to github

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template> 
+    <div class="header"> 
+        <h1>NotFlix</h1>
+    </div>
+    <div class="buttons">
+        <RouterLink to="/register" class="button">Register</RouterLink>
+        <RouterLink to="/login" class="button">Login</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: black; 
+  padding: 20px;  
+  width: 100%;  
+  position: fixed;  
+  top: 0;  
+  left: 0;  
+  z-index: 1000;  
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black; 
+  padding: 20px;
+  margin-top: 80px; 
+}
+
+.button {
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: #092ae5; 
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  transition: background-color 0.3s;
+}
+
+.button:hover {
+  background-color: #092ae5; 
+}
+
+h1 {
+  color: white; 
+  background-color: black;
+  font-family: 'Arial', sans-serif;
+  font-size: 3rem; 
+  font-style: italic;
+  text-align: center;
+  margin: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Hero from '../components/Hero.vue';
+import Feature from '../components/Feature.vue';
+import Footer from '../components/Footer.vue';
+
+</script>
+
+<template>
+  <Header/>
+  <Hero/>
+  <Feature/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
